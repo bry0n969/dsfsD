9c01b5a824c1937e5353c4a73b50401b6f7cf88b
bry0n969 <bry0n969@gmail.com>
2017-02-07 04:58:25 -0600

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -1,27 +1,43 @@
-<?
-// This is a template for a PHP scraper on morph.io (https://morph.io)
-// including some code snippets below that you should find helpful
+import scraperwiki
 
-// require 'scraperwiki.php';
-// require 'scraperwiki/simple_html_dom.php';
-//
-// // Read in a page
-// $html = scraperwiki::scrape("http://foo.com");
-//
-// // Find something on the page using css selectors
-// $dom = new simple_html_dom();
-// $dom->load($html);
-// print_r($dom->find("table.list"));
-//
-// // Write out to the sqlite database using scraperwiki library
-// scraperwiki::save_sqlite(array('name'), array('name' => 'susan', 'occupation' => 'software developer'));
-//
-// // An arbitrary query against the database
-// scraperwiki::select("* from data where 'name'='peter'")
+# Blank Python
 
-// You don't have to do things with the ScraperWiki library.
-// You can use whatever libraries you want: https://morph.io/documentation/php
-// All that matters is that your final data is written to an SQLite database
-// called "data.sqlite" in the current working directory which has at least a table
-// called "data".
-?>
+import scraperwiki           
+import lxml.html           
+
+urls = ["http://www.ebay.com/sch/m.html?_nkw=&_armrs=1&_from=&_ssn=offroadbelts&_pgn=2&_skc=200&rt=nc"]
+
+max_pages = 10000
+
+for wurl in urls:
+    curr_url = wurl 
+    page_idx = 1
+    
+    while page_idx <= max_pages :
+        error = True
+        while error:
+            try:    
+                html = scraperwiki.scrape(curr_url)
+                root = lxml.html.fromstring(html)
+                
+                for tr in root.cssselect("div[class='ittl'] a"):    
+                    url = tr.get("href")
+                    html = scraperwiki.scrape(url)                
+                    if html.find("channeladvisor_poweredby-en.gif") != -1 :
+                        root2 = lxml.html.fromstring(html)
+                        for mname in root2.cssselect("span[class='mbg-nw']"):
+                            data = {
+                                'url': url,
+                                'merchant_name': mname.text
+                            }
+                            scraperwiki.sqlite.save(unique_keys=['url'],data=data)
+    
+                for next_page in root.cssselect("td[class='botpg-next'] a"):
+                    print curr_url 
+                    curr_url = next_page.get("href")
+                    page_idx = page_idx +1             
+
+                error = False
+            except:
+                print 'error'
+error = True
